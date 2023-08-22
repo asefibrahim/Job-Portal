c73cba683579f3a4a2d0797516226ea3a2c509c8
asefibrahim <strongman01666@gmail.com>
2023-08-22 19:22:30 +0600

    Error page included

--- src/pages/companies/MainCOmpanies/MainCOmpanies.jsx
+++ src/pages/companies/MainCOmpanies/MainCOmpanies.jsx
@@ -1,57 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import CompaniesSection from '../../Home/homeChild/CompaniesSection/CompaniesSection';
-import SingleJob from '../../Home/homeChild/jobsSection/SingleJob';
-import Loading from '../../../components/Loading';
+import React, { useEffect, useState } from "react";
+import CompaniesSection from "../../Home/homeChild/CompaniesSection/CompaniesSection";
+import SingleJob from "../../Home/homeChild/jobsSection/SingleJob";
+import Loading from "../../../components/Loading";
 
 const MainCOmpanies = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.slice(3, 6));
+        setLoading(false); // Set loading state to false after data is fetched
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false); // Also set loading state to false in case of an error
+      });
+  }, []);
 
+  return (
+    <div>
+      <CompaniesSection />
 
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => {
-                setData(data.slice(3, 6));
-                setLoading(false); // Set loading state to false after data is fetched
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-                setLoading(false); // Also set loading state to false in case of an error
-            });
-    }, []);
+      <div className="text-center font-extrabold text-4xl my-12 text-red-500">
+        Jobs
+      </div>
 
+      <div>
+        {loading ? (
+          <Loading size={3} />
+        ) : (
+          data.map((singleJob) => (
+            <SingleJob key={singleJob.id} singleJob={singleJob} />
+          ))
+        )}
+      </div>
 
-
-    return (
-        <div>
-            <CompaniesSection/>
-
-
-<div className='text-center font-extrabold text-4xl my-12 text-red-500'>
-    Jobs 
-</div>
-
-
-            <div>
-                {loading?<Loading size={3}/>:
-                    data.map(singleJob => <SingleJob key={singleJob.id}  singleJob={singleJob} />)
-                }
-
-
-
-
-
-            </div>
-
-<div>
-
-
-
-</div>
-
-        </div>
-    );
+      <div></div>
+    </div>
+  );
 };
 
 export default MainCOmpanies;
